1669db8affbf8037d9da8cc9f48c6b3df271a9d5
Larry <raijin22@outlook.com>
2022-07-19 09:42:48 -0400

    JS practice

---
+++ JS_practice.js
@@ -0,0 +1,17 @@
+/*let x = 'Random words.';
+
+for(let i = 0; i < x.length; i++) {
+  console.log(`The index number is ${i} and the letter is ${x.charAt(i)}`);
+}
+*/
+/*
+function roll() {
+  return Math.floor(Math.random() *6) + 1;
+}
+
+function dice(die) {
+  console.log(`You rolled a ${die}`);
+}
+
+dice(roll());
+*/
